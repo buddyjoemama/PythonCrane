57457de9e8ceddea681ae42da7dc740b0dc22c32
Brian Bacon <brian.bacon01@gmail.com>
2019-09-16 21:59:19 -0400

    intial checkin

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from picamera import PiCamera
+from time import sleep
+
+app = Flask(__name__)
+camera = PiCamera()
+
+@app.route('/')
+def index():
+    return render_template('Index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
