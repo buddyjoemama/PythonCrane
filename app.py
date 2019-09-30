2908869b53c94ced043a96f85a5987430f162515
Brian Bacon <brian.bacon01@gmail.com>
2019-09-30 19:14:15 -0400

    updates

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-from picamera import PiCamera
-from time import sleep
+import serial
 
 app = Flask(__name__)
+ser = serial.Serial('/dev/ttyACM0')
 
 @app.route('/')
 def index():
     return render_template('Index.html')
+
+@app.route('/control')
+def control():
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True) 
