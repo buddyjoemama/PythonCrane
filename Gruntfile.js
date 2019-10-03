53fa8e02f7c84fc1b7ef67af99b875c58b381b96
BRIAN BACON <Brian.Bacon@xerox.com>
2019-10-02 22:39:42 -0400

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,31 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
+    var config = {
+        libs: ['node_modules/angular/angular.min.js',
+                'node_modules/ui-bootstrap4/dist/ui-bootstrap-tpls.js']
+    };
 
+    var outFiles = {
+        'templates/dist/crane.libs.js': ['<%=config.libs%>']
+    };
+
+    grunt.initConfig({
+        config: config,
+
+        uglify: {
+            default: {
+                options: {
+                    beautify: true,
+                    mangle: false,
+                    compress: false
+                },
+                files: outFiles
+            }
+        }
     });
   
-   // grunt.loadNpmTasks('grunt-contrib-jshint');
-   // grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
   
-   // grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['uglify:default']);
   
   };
