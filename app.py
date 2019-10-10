2ee3b1e31c818a6c4111a1817f5a76eff68be351
Brian Bacon <brian.bacon01@gmail.com>
2019-10-09 21:44:32 -0400

    Updated python

--- app.py
+++ app.py
@@ -4,21 +4,9 @@ import serial
 app = Flask(__name__)
 #ser = serial.Serial('/dev/ttyACM0')
 
-@app.route('/js/<path:path>')
-def get_js(path):
-    return send_from_directory('templates/js', path)
-
-@app.route('/html/<path:path>')
-def get_html(path):
-    return send_from_directory('templates', path)
-
-@app.route('/')
-def index():
-    return render_template('Index.html')
-
-@app.route('/py/control')
+@app.route('/py/control/')
 def control():
-    return None
+    return "Hello"
 
 if __name__ == '__main__':
     app.run(debug=True) 
