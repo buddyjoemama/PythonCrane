59fa0387be8daaaa5b422658f7a7faa2b980f89f
Brian Bacon <brian.bacon01@gmail.com>
2019-10-18 22:37:08 -0400

    Added material

--- angular-crane/src/app/app.module.ts
+++ angular-crane/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ControlPanelComponent } from './control-panel/control-panel.component';
 import { HomeComponent } from './home/home.component';
 import { CameraViewComponent } from './camera-view/camera-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CameraViewComponent } from './camera-view/camera-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
