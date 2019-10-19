067afa4519cc31e55a014466e64d2aef88a7ff00
Brian Bacon <brian.bacon01@gmail.com>
2019-10-19 09:07:36 -0400

    updates

--- angular-crane/src/app/app-routing.module.ts
+++ angular-crane/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '', component: HomeComponent
   },
   {
-    path: 'view', component: CameraViewComponent
+    path: 'view/:id', component: CameraViewComponent
   }
 ];
 
