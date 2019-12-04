6ff79cba4d44ceb84cb5e0bc72e1c13579dfcb35
Brian Bacon <brian.bacon01@gmail.com>
2019-12-03 22:00:53 -0500

    commit

--- static/app.js
+++ static/app.js
@@ -1,4 +1,11 @@
 angular.module('craneStatic.app', [])
+    .controller('craneController', function() {
+      var ctrl = this;
+
+        ctrl.action = function(dir) {
+            console.log(dir);
+        }
+    })
     .run(function() {
         console.log("Startup");
     });
